e6152c4f8023894c691432deb9aa935c65fe1c09
Hridoy <ridoy.java@gmail.com>
2023-09-13 14:14:21 +0600

    Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import ListGroup from './components/ListGroup'
 import 'bootstrap/dist/css/bootstrap.css'
 function App() {
-  const items = ["New York", "Islamabad", "Dhaka", "Sylhet", "London"];
+  const items = ["Rupgonj", "Islamabad", "Dhaka", "Sylhet", "London"];
 
   return (
     <>
       < ListGroup items={items} heading='City'/>
-    
     </>
   )
 }
