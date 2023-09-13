0c7f5bb9e4fff65bf56abc6bac1c7d823a58be6c
Hridoy <ridoy.java@gmail.com>
2023-09-13 17:45:16 +0600

    Building Components sort work

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 // import ListGroup from './components/ListGroup'
 import 'bootstrap/dist/css/bootstrap.css'
 import Button from './components/Button'
+import Alert from './components/Alert'
+import { useState } from 'react'
 //import Alert from './components/Alert'
 function App() {
 //   const items = ["Rupgonj", "Islamabad", "Dhaka", "Sylhet", "Hridoy","Hossain"];
 // const handleSelectItem = (item:string) =>{
 //   console.log(item);
 // }
+
+const [visibility,setVisivility] = useState(false);
   return (
     <>
       {/* < ListGroup items={items} heading='City'onSelectItem={handleSelectItem}/> */}
       {/* <Alert>
         Hello  <span>World</span>
       </Alert> */}
-
-    <Button color='danger' onClick={()=>console.log('Clicked')}>
+    {visibility && <Alert onClose={()=>setVisivility(false)}>My Alert</Alert>}
+    <Button color='danger' onClick={()=>setVisivility(true)}>
       My Button
     </Button>
     </>
