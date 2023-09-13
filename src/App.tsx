8753350a8a779fa1ed2f63b26255a3336fee7cf7
Hridoy <ridoy.java@gmail.com>
2023-09-13 16:35:59 +0600

    passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import ListGroup from './components/ListGroup'
+// import ListGroup from './components/ListGroup'
 import 'bootstrap/dist/css/bootstrap.css'
+import Alert from './components/Alert'
 function App() {
-  const items = ["Rupgonj", "Islamabad", "Dhaka", "Sylhet", "Hridoy","Hossain"];
-const handleSelectItem = (item:string) =>{
-  console.log(item);
-}
+//   const items = ["Rupgonj", "Islamabad", "Dhaka", "Sylhet", "Hridoy","Hossain"];
+// const handleSelectItem = (item:string) =>{
+//   console.log(item);
+// }
   return (
     <>
-      < ListGroup items={items} heading='City'onSelectItem={handleSelectItem}/>
+      {/* < ListGroup items={items} heading='City'onSelectItem={handleSelectItem}/> */}
+      <Alert>
+        Hello  <span>World</span>
+      </Alert>
     </>
   )
 }
